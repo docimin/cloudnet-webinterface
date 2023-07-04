a8da37335d4e3a0747e0f15e9b415b6406386c44
Faye <dominic@stilma.net>
2023-07-04 16:54:13 +0200

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+      domains: ['cdn.discordapp.com', 'fayevr.dev', 'fayevr-dev.pages.dev'],
+    },
+    experimental: {
+      appDir: true,
+    },
+}
 
 module.exports = nextConfig
