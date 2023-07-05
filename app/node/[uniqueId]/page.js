ff436d559885c4e7ad6804118515e38921c5af51
Faye <dominic@stilma.net>
2023-07-05 19:13:19 +0200

    some stuff

--- app/node/[uniqueId]/page.js
+++ app/node/[uniqueId]/page.js
@@ -1,16 +1,33 @@
-function ClusterNodeEditPage({ node }) {
-    // render the edit form using the `node` prop
-  }
-  
-  export async function getServerSideProps(context) {
-    const { uniqueId } = context.query;
-    // make a GET request to `/node/${uniqueId}` to fetch the node information
-    const node = await fetch(`/node/${uniqueId}`).then((res) => res.json());
+import NodePage from '@/components/nodePage';
+
+export default function Node({ node }) {
+  return <NodePage node={node} />;
+}
+
+export async function getServerSideProps(context) {
+  const { uniqueId } = context.query;
+  const token = getCookie('token');
+  if (token) {
+    const response = await fetch(`https://cors.fayevr.dev/proxy-api.fayevr.dev/api/v2/cluster/${uniqueId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
     return {
       props: {
-        node,
-      },
+        node: data.node
+      }
     };
   }
-  
-  export default ClusterNodeEditPage;
+  return {
+    props: {
+      node: {}
+    }
+  };
+}
---
+++ components/nodePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NodePage({ node }) {
+  return (
+    <div>
+      <h1>hi</h1>
+      <p>Status: </p>
+      <p>Memory: </p>
+      <p>Version: </p>
+      <p>Type: </p>
+    </div>
+  );
+}
