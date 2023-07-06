10333c83161faf65b1741bb44ad62e7f6c20b077
Faye <dominic@stilma.net>
2023-07-06 04:33:18 +0200

    edge runtime

--- app/nodes/[uniqueId]/page.js
+++ app/nodes/[uniqueId]/page.js
@@ -1,5 +1,7 @@
 import NodePage from '@/components/nodes/nodePage';
 
+export const runtime = 'edge';
+
 export default function Node({ node }) {
   return <NodePage node={node} />;
 }
--- app/services/[uniqueId]/page.js
+++ app/services/[uniqueId]/page.js
@@ -1,5 +1,7 @@
 import ServicePage from '@/components/services/servicePage';
 
+export const runtime = 'edge';
+
 export default function Service({ service }) {
   return <ServicePage service={service} />;
 }
