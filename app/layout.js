993833e6cb32194881818993d5d464448c0a5f9e
Faye <dominic@stilma.net>
2023-07-06 17:14:22 +0200

    fix svg errors

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,9 @@ import {
   FolderIcon,
   HomeIcon,
   UsersIcon,
-  XMarkIcon
+  XMarkIcon,
+  MoonIcon,
+  SunIcon
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -224,37 +226,17 @@ export default function RootLayout({ children }) {
                   <img className="h-8 w-auto" src="/icon-256.png" alt="image" />
                 </div>
                 {colorTheme === 'light' ? (
-                  <svg
+                    <><SunIcon
                     onClick={() => setTheme('light')}
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="white"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-                    />
-                  </svg>
+                    className="h-6 w-6 text-white"
+                    aria-hidden="true" />
+                    </>
                 ) : (
-                  <svg
+                  <><MoonIcon
                     onClick={() => setTheme('dark')}
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="black"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                    />
-                  </svg>
+                    className="h-6 w-6 text-black"
+                    aria-hidden="true" />
+                    </>
                 )}
                 <nav className="flex flex-1 flex-col">
                   <ul role="list" className="flex flex-1 flex-col gap-y-7">
