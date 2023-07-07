9a9d7bace79b67b62fc6226bf125e9615cdab2b2
Faye <dominic@stilma.net>
2023-07-07 10:26:34 +0200

    websocket test

--- components/services/serviceConsole.js
+++ components/services/serviceConsole.js
@@ -1,65 +1,29 @@
 'use client';
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import ServiceConsole from '@/components/services/serviceConsole';
+import { useEffect } from 'react';
+import io from 'socket.io-client';
 
-export default function Service() {
-  const [services, setServices] = useState([]);
-  const [error, setError] = useState('');
-
-  function getCookie(name) {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.startsWith(name + '=')) {
-        return cookie.substring(name.length + 1);
-      }
-    }
-    return null;
-  }
-
+const WebSocketClient = ({ onDataReceived }) => {
   useEffect(() => {
-    const token = getCookie('token');
-    const uniqueId = window.location.pathname.split('/').pop();
-    if (!token) {
-      window.location.href = '/auth';
-      return;
-    }
-    if (token) {
-      const ws = new WebSocket(
-        `${process.env.NEXT_PUBLIC_PROXY_URL.replace(
-          /^http/,
-          'ws'
-        )}/service/${uniqueId}/liveLog`
-      );
-      ws.onopen = () => {
-        console.log('WebSocket connection opened');
-      };
-      ws.onmessage = (event) => {
-        console.log(`Received message: ${event.data}`);
-      };
-      ws.onerror = (error) => {
-        console.error(`WebSocket error: ${error}`);
-        console.log(error);
-      };
-      ws.onclose = () => {
-        console.log('WebSocket connection closed');
-      };
-    }
-  }, []);
+    const socket = io('ws://217.160.104.158:2812/api/v2/service/test-1/liveLog');
 
-  return (
-    <>
-    <span>Console:</span>
-    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-      <div className="px-4 py-5 sm:px-6">
-        {services.map((service) => (
-          <ServiceConsole key={service.id} service={service} />
-        ))}
-        {error && <p>{error}</p>}
-      </div>
-      <div className="px-4 py-5 sm:p-6">{/* Content goes here */}</div>
-    </div>
-    </>
-  );
-}
+    socket.on('connect', () => {
+      console.log('WebSocket connected');
+    });
+
+    socket.on('data', (data) => {
+      onDataReceived(data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('WebSocket disconnected');
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [onDataReceived]);
+
+  return null;
+};
+
+export default WebSocketClient;
