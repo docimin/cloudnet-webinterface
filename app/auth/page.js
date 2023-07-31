2eb2f609b52bc6d9c43b876d379f1fabcf004c10
Faye <dominic@stilma.net>
2023-07-31 09:15:00 +0200

    fix logo

--- app/auth/page.js
+++ app/auth/page.js
@@ -77,7 +77,7 @@ export default function Auth() {
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
               className="mx-auto h-10 w-auto"
-              src="/icon-256.png"
+              src="/logo.svg"
               alt="Sushi Roll"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-light-color">
