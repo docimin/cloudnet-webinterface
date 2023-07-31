8e4c3db98c5c731a3a1860d8598f7a31bbc91663
Faye <dominic@stilma.net>
2023-07-31 08:13:26 +0200

    fix task url

--- components/tasks/tasksInfo.js
+++ components/tasks/tasksInfo.js
@@ -8,12 +8,23 @@ export default function Nodes() {
 
   useEffect(() => {
     const token = getCookie('token');
+    const address = getCookie('address');
     if (!token) {
       window.location.href = '/auth';
       return;
     }
     if (token) {
-      fetch(process.env.NEXT_PUBLIC_DEV_PROXY_URL + '/task', {
+      //fetch(process.env.NEXT_PUBLIC_DEV_PROXY_URL + '/service', {
+      const apiUrl = address.includes('/api/v2')
+        ? ''
+        : process.env.NEXT_PUBLIC_API_URL;
+
+        const domainurl = address.includes('localhost' || '127.0.0.1')
+        ? ''
+        : `${process.env.NEXT_PUBLIC_CORS_PROXY_URL}/`;
+
+      fetch(
+        `${domainurl}${address}${apiUrl}/task`, {
         headers: {
           Authorization: `Bearer ${token}`,
           'Content-Type': 'application/json'
