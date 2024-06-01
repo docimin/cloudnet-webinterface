52abbc248543e4f1f172576860c114036671f339
Faye <dominic@stilma.net>
2024-06-01 13:35:10 +0200

    fix build

---
+++ src/app/[lang]/(dashboard)/dashboard/nodes/[nodeId]/page.tsx
@@ -0,0 +1,9 @@
+import PageLayout from '@/components/pageLayout'
+
+export default function NodePage() {
+  return (
+    <PageLayout title={'Node'}>
+      <div>Node Page</div>
+    </PageLayout>
+  )
+}
