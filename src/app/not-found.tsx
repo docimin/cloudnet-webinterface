ad9c60828f635b70963ebe21b50f7262188fa05f
Faye <faye@fayevr.dev>
2024-08-07 02:51:45 +0200

    fix import

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { useRouter } from '@/navigation'
+import { useRouter } from 'next/navigation'
 import { cn } from '@/lib/utils'
 
 export default function NotFoundComponent() {
