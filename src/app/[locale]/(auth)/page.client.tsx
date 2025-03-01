f152f78057f83185e6b9e54969fbbace980b1c22
Faye <faye@fayevr.dev>
2025-03-01 19:13:04 +0100

    change text to username

--- src/app/[locale]/(auth)/page.client.tsx
+++ src/app/[locale]/(auth)/page.client.tsx
@@ -82,7 +82,7 @@ export default function Client() {
               <div className="grid gap-2 text-center">
                 <h1 className="text-3xl font-bold">Login</h1>
                 <p className="text-balance text-muted-foreground">
-                  Enter your email below to login to your account
+                  Enter your username below to login to your account
                 </p>
               </div>
               <div className="grid gap-4">
