368f313ec22ef4ee6460f58b75f0dbeadfab8c61
Faye <faye@fayevr.dev>
2025-03-25 21:36:16 +0100

    fix .split server error

--- src/lib/server-calls.ts
+++ src/lib/server-calls.ts
@@ -4,7 +4,7 @@ import { headers } from 'next/headers'
 export async function getCookies() {
   const headersList = await headers()
   const cookieHeader = headersList.get('cookie')
-  if (!cookieHeader) return {}
+  if (!cookieHeader || cookieHeader.trim() === '') return {}
   return cookieHeader.split('; ').reduce((res, item) => {
     const data = item.split('=')
     return { ...res, [data[0]]: data[1] }
