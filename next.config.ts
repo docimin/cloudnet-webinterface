9332b025828e240a5b681d80601bcaebfe332be0
Faye <faye@fayevr.dev>
2026-01-12 16:41:12 +0100

    chore: update packages

--- next.config.ts
+++ next.config.ts
@@ -94,15 +94,6 @@ const sentryOptions: SentryBuildOptions = {
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of
   // client-side errors will fail.
   tunnelRoute: '/api/monitoring',
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors.
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: false
 }
 
 const sentryNextConfig = withSentryConfig(nextConfig, sentryOptions)
